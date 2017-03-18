e776b1d651679ad8bd33653f6160360d73f1e54b
Daniel Malik <daniel.malik.malik@gmail.com>
2017-03-18 20:30:24 +0100

    test views

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 from .models import Bucketlist
 
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 # Create your tests here.
 
 class ModelTestCase(TestCase):
     def setUp(self):
-        self.bucketlist_name = "Write world class code"
+        self.bucketlist_name = "100kg Clean and Jerk"
         self.bucketlist = Bucketlist(name=self.bucketlist_name)
     
     def test_model_can_create_a_bucketlist(self):
@@ -13,3 +16,18 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Bodyweight Snatch'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format = "json"
+        )
+    def test_api_can_create_a_bucketlist(self):
+
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
